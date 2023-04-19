108ce98e3085ed9544f0ee8d96a52637de1a88c5
rassafel <rassafel@gmail.com>
2023-04-19 12:09:44 +0400

    Add initial data sql.

---
+++ spring-learn/src/main/resources/data.sql
@@ -0,0 +1,10 @@
+insert into author(id,name) values (1, 'John');
+insert into author(id,name) values (2, 'Ivan');
+insert into author(id,name) values (3, 'Egor');
+insert into author(id,name) values (4, 'Ilya');
+
+insert into post(author_id, text, views) values (1, 'Test 1', 0);
+insert into post(author_id, text, views) values (1, 'Test 2', 0);
+insert into post(author_id, text, views) values (2, 'Ivan Test', 0);
+insert into post(author_id, text, views) values (3, 'Egor', 0);
+insert into post(author_id, text, views) values (1, 'Test 3', 0);
